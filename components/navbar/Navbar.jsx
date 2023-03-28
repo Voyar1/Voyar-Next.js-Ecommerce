164898ee423967e9f3bc75a063afeaae5b0d5e50
Arkadiusz <areq12x@gmail.com>
2023-03-28 19:10:59 +0200

    ADD mobile menu Modal

--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -1,9 +1,17 @@
 import Link from "next/link";
 import styles from "./navbar.module.css";
 import { BiSearch, BiCart, BiUser, BiMenu } from "react-icons/bi";
+import { MdClose } from "react-icons/md";
 import useMediaQuery from "@/hooks/useMediaQuery";
+import { useState } from "react";
+
 const Navbar = () => {
   const isAboveMedium = useMediaQuery("(min-width: 1060px)");
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenuHandler = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
     <>
@@ -19,24 +27,56 @@ const Navbar = () => {
             <Link href="category/acessories">ACCESSORIES</Link>
           </div>
           <div className={styles.navIcons}>
-            <BiSearch />
-            <BiUser />
-            <BiCart />
+            <button>
+              <BiSearch />
+            </button>
+            <button>
+              <BiUser />
+            </button>
+            <button>
+              <BiCart />
+            </button>
           </div>
         </nav>
       ) : (
         // MOBILE NAV
         <nav className={styles.mobileNav}>
           <div className={styles.navIcons}>
-            <BiUser />
-            <BiCart />
+            <button>
+              <BiUser />
+            </button>
+            <button>
+              <BiCart />
+            </button>
           </div>
           <div className={`${styles.mobileLogo} ${styles.logo}`}>
             <Link href="/">VOYAR</Link>
           </div>
           <div className={styles.mobileNavMenu}>
-            <BiSearch />
-            <BiMenu />
+            <button>
+              <BiSearch />
+            </button>
+            <button onClick={toggleMenuHandler}>
+              <BiMenu />
+            </button>
+          </div>
+          {/* MOBILE NAV MODAL */}
+
+          <div
+            className={`${styles.mobileNavModal} ${
+              isOpen ? styles.modalIsOpen : ""
+            }`}
+          >
+            <div className={styles.closeBtnWrapper}>
+              <button className={styles.closeBtn} onClick={toggleMenuHandler}>
+                <MdClose />
+              </button>
+            </div>
+            <div className={`${styles.mobileNavItems} ${styles.navItems} `}>
+              <Link href="/category/women">WOMEN</Link>
+              <Link href="category/men">MEN</Link>
+              <Link href="category/acessories">ACCESSORIES</Link>
+            </div>
           </div>
         </nav>
       )}
