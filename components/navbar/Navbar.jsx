9e08da58f2f3f7e8bfeef90d1ad07d248c0d8b99
Arkadiusz <areq12x@gmail.com>
2023-03-28 18:35:31 +0200

    ADD responsive nav and responsive Hero section

--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -1,23 +1,46 @@
 import Link from "next/link";
 import styles from "./navbar.module.css";
-import { BiSearch, BiCart, BiUser } from "react-icons/bi";
+import { BiSearch, BiCart, BiUser, BiMenu } from "react-icons/bi";
+import useMediaQuery from "@/hooks/useMediaQuery";
 const Navbar = () => {
+  const isAboveMedium = useMediaQuery("(min-width: 1060px)");
+
   return (
-    <nav className={styles.navbar}>
-      <div className={styles.logo}>
-        <Link href="/">VOYAR</Link>
-      </div>
-      <div className={styles.navItems}>
-        <Link href="/category/women">WOMEN</Link>
-        <Link href="category/men">MEN</Link>
-        <Link href="category/acessories">ACCESSORIES</Link>
-      </div>
-      <div className={styles.navIcons}>
-        <BiSearch />
-        <BiUser />
-        <BiCart />
-      </div>
-    </nav>
+    <>
+      {isAboveMedium ? (
+        // DESKTOP NAV
+        <nav className={styles.navbar}>
+          <div className={styles.logo}>
+            <Link href="/">VOYAR</Link>
+          </div>
+          <div className={styles.navItems}>
+            <Link href="/category/women">WOMEN</Link>
+            <Link href="category/men">MEN</Link>
+            <Link href="category/acessories">ACCESSORIES</Link>
+          </div>
+          <div className={styles.navIcons}>
+            <BiSearch />
+            <BiUser />
+            <BiCart />
+          </div>
+        </nav>
+      ) : (
+        // MOBILE NAV
+        <nav className={styles.mobileNav}>
+          <div className={styles.navIcons}>
+            <BiUser />
+            <BiCart />
+          </div>
+          <div className={`${styles.mobileLogo} ${styles.logo}`}>
+            <Link href="/">VOYAR</Link>
+          </div>
+          <div className={styles.mobileNavMenu}>
+            <BiSearch />
+            <BiMenu />
+          </div>
+        </nav>
+      )}
+    </>
   );
 };
 
